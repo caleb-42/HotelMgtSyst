16e8cdce969937ad8b3ef3eec9c821cb5484e8e5
tegovona <tegovona@gmail.com>
2018-10-29 11:20:23 +0100

    edited frontdesk_room_by_category to include reservations array

--- php1/front_desk/frontdesk_rooms_by_category.php
+++ php1/front_desk/frontdesk_rooms_by_category.php
@@ -3,27 +3,37 @@
 
   $category = $_POST["category"];
 
-  $get_bookings_sql = "SELECT * FROM frontdesk_rooms WHERE booked = 'NO' AND category = '$category'";
-  $get_bookings_result = mysqli_query($dbConn, $get_bookings_sql);
-  $get_bookings_array = [];
+  $get_rooms_sql = "SELECT * FROM frontdesk_rooms WHERE booked = 'NO' AND category = '$category'";
+  $get_rooms_result = mysqli_query($dbConn, $get_rooms_sql);
+  $get_rooms_array = [];
 
-  function get_all_frontdesk_bookings($bookings_result, $bookings_array) {
-    $bookings_array = [];
+  function get_all_frontdesk_rooms($rooms_result, $rooms_array, $connDB) {
+    $rooms_array = [];
 
-    if (mysqli_num_rows($bookings_result) > 0){
- 	  while($rows = mysqli_fetch_assoc($bookings_result)) {
- 		$bookings_array[] = $rows;
- 	  }
- 	  $get_bookings_json = json_encode($bookings_array);
- 	  return $get_bookings_json;
+    if (mysqli_num_rows($rooms_result) > 0){
+    while($rows = mysqli_fetch_assoc($rooms_result)) {
+      $room_id = $rows["room_id"];
+      $reservation_query = "SELECT * FROM frontdesk_reservations WHERE room_id = '$room_id'";
+      $get_reservations_result = mysqli_query($connDB, $reservation_query);
+      if (mysqli_num_rows($get_reservations_result) > 0) {
+        $reservations_rows = [];
+        while ($rows_reservations = mysqli_fetch_assoc($get_reservations_result)) {
+           $reservations_rows[] = $rows_reservations;
+         }
+         $rows["reservations"] = $reservations_rows;
+      }
+      $rooms_array[] = $rows;
+    }
+    $get_rooms_json = json_encode($rooms_array);
+    return $get_rooms_json;
     }
   }
 
-  $frontdesk_bookings = get_all_frontdesk_bookings($get_bookings_result, $get_bookings_array);
+  $frontdesk_rooms = get_all_frontdesk_rooms($get_rooms_result, $get_rooms_array, $dbConn);
 
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	echo $frontdesk_bookings;
+  if ($_SERVER["REQUEST_METHOD"] += "POST") {
+  echo $frontdesk_rooms;
   } else {
-  	echo "UNAUTHORIZED ACCESS";
-  } 
+    echo "UNAUTHORIZED ACCESS";
+  }
 ?>
