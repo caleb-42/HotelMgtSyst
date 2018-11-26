310672c8452d13f51f3ed671f21f0427174122ea
EWERE <ewiscobaba@gmail.com>
2018-11-26 17:18:04 +0100

    revenue made

---
+++ accounts/assets/js/revenue/controllers.js
@@ -0,0 +1,47 @@
+revenueApp.controller("revenue", ["$rootScope", "$scope", 'jsonPost', '$filter',  function ($rootScope, $scope, jsonPost, $filter) {
+    $scope.tabnav = {
+        navs: {
+            Bookings: {
+                name: 'Bookings',
+                options: {
+                    rightbar : false
+                }
+            }
+        },
+        selected: {
+            name: 'Bookings',
+            options: {
+                rightbar : false
+            }
+        },
+        selectNav: function (navname) {
+            $scope.tabnav.selected = $scope.tabnav.navs[navname];
+        }
+    };
+    $scope.rightSidebar = {
+        itemlist: function () {
+            return {
+                jsonfunc: jsonPost.data("../php1/restaurant_bar/admin/list_sessions.php", {})
+            }
+        },
+        subclass: {
+
+        }
+    };
+
+    
+}]);
+
+revenueApp.controller("bookinghistory", ["$rootScope", "$scope", 'jsonPost', '$filter', function ($rootScope, $scope, jsonPost, $filter) {
+    $scope.listbookings = {
+        itemlist: function () {
+            return {
+                jsonfunc: jsonPost.data("../php1/front_desk/list_bookings.php", {})
+            }
+        }
+    }
+
+}]);
+
+
+
