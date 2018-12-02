ff068819229269c8f485ae0d2328fa495c5464f6
teevona <tegovona@gmail.com>
2018-12-02 18:38:19 +0100

    some expense range shii

--- php1/accounts/list_expenses.php
+++ php1/accounts/list_expenses.php
@@ -5,15 +5,26 @@
   $get_items_result = mysqli_query($dbConn, $get_items_sql);
   $get_items_array = [];
 
-  function get_all_account_expenses($items_result, $items_array) {
-    $items_array = [];
+  function get_all_account_expenses($expense_result, $expense_array) {
+    $expense_array = [];
+    $expense_total = 0;
+    $expense_worth = 0;
+    $expense_balance = 0;
 
-    if (mysqli_num_rows($items_result) > 0){
- 	  while($rows = mysqli_fetch_assoc($items_result)) {
- 		$items_array[] = $rows;
- 	  }
- 	  $get_items_json = json_encode($items_array);
- 	  return $get_items_json;
+    if (mysqli_num_rows($expense_result) > 0){
+    while($rows = mysqli_fetch_assoc($expense_result)) {
+      $expense_array[] = $rows;    
+      $expense_total = $expense_total + intval($rows["amount_paid"]);
+      $expense_balance = $expense_balance + intval($rows["balance"]);
+      $expense_worth = $expense_worth + intval($rows["txn_worth"]);
+    } 
+    $expense["expense_array"] = $expense_array;
+    $expense["expense_total"] = $expense_total;
+    $expense["expense_balance"] = $expense_balance;
+    $expense["expense_worth"] = $expense_worth;
+
+    $get_expense_json = json_encode($expense);
+    return $get_expense_json;
     }
   }
 
